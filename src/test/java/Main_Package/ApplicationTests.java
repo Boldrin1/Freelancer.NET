545f9249988cc0b61fed94c08aa409fe7d367f83
Boldrin1 <srzip74@gmail.com>
2024-09-14 12:28:26 -0300

    Primeiro Commit - 14/09/2024

---
+++ src/test/java/Main_Package/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Main_Package;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
