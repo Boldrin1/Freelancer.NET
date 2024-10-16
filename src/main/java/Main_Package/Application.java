71c9d61d1c8850d89f174bf06eaaec80c82e1d9a
Boldrin1 <srzip74@gmail.com>
2024-10-16 11:50:51 -0300

    16/10

--- src/main/java/Main_Package/Application.java
+++ src/main/java/Main_Package/Application.java
@@ -13,22 +13,30 @@ import Main_Package.repository.ClienteRepository;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-    @Autowired
-    private ClienteRepository clienteRepository;
+   
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+   /* @Autowired
+    private ClienteRepository clienteRepository;
     @Override
     public void run(String... args) throws Exception {
         Cliente cliente = new Cliente();
-        cliente.setNome("Fernando");
-        cliente.setEmail("Roberto@gmail.com");
-        cliente.setSenha("123");
-        cliente.setDataNascimento(new Date());
-        cliente.setSexo("Masculino");
         cliente.setCPF("12");
+        cliente.setDataNascimento(new Date());
+        cliente.setEmail("Roberto@gmail.com");
+        cliente.setNome("Fernando");
+        cliente.setSenha("123");
+        cliente.setSexo("Masculino");              
+        cliente.setTelefone("44 99909393");
         clienteRepository.save(cliente);
-    }
+    }*/
 }
