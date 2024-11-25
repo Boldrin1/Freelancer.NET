a0c33cf085eb2775ccc6bd17e9b8ad195ba4b543
Boldrin1 <srzip74@gmail.com>
2024-11-25 17:46:48 -0300

    25/11/2024

--- src/main/java/Main_Package/config/Validacao.java
+++ src/main/java/Main_Package/config/Validacao.java
@@ -6,19 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import Main_Package.repository.ClienteRepository;
 import Main_Package.repository.FreelancerRepository;
 
 @RestController
-@RequestMapping("/api/freelancers")
+@RequestMapping("/api/usuarios")
 public class Validacao {
 
     @Autowired
     private FreelancerRepository freelancerRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @GetMapping("/check-email")
     public ResponseEntity<Boolean> checkEmail(@RequestParam String email) {
-        boolean emailExists = freelancerRepository.findByEmail(email).isPresent();
-        System.out.println("Verificando e-mail: " + emailExists);
+        boolean emailExists = freelancerRepository.findByEmail(email).isPresent() ||
+                              clienteRepository.findByEmail(email).isPresent();
         return ResponseEntity.ok(emailExists);
     }
 }
