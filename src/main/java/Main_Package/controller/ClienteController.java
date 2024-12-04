894020a1ac632e910dfb56d055c220299d957757
Boldrin1 <srzip74@gmail.com>
2024-12-04 17:59:59 -0300

    04/12

--- src/main/java/Main_Package/controller/ClienteController.java
+++ src/main/java/Main_Package/controller/ClienteController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -176,14 +175,6 @@ public class ClienteController {
 	            .header(HttpHeaders.LOCATION, redirecionarUrl)
 	            .build();
 	}
-
-	
-	@DeleteMapping("/perfil/{id}")
-	public String excluirCliente(Long id) {
-		return "redirect:/create-count";
-	}
-	
-	
 	
 	@GetMapping("/inbox/{id}")
 	public String clienteInbox(@PathVariable("id") Long clienteId, Model model) {
--- src/main/java/Main_Package/controller/FreelancerController.java
+++ src/main/java/Main_Package/controller/FreelancerController.java
@@ -5,14 +5,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import Main_Package.model.AreaDeInteresse;
 import Main_Package.model.Cliente;
 import Main_Package.model.Curriculo;
@@ -161,16 +159,6 @@ public class FreelancerController {
 		return "redirect:/usuario/freelancer/perfil/" + freelancerAtualizado.getId();
 	}
 	
-
-	
-	@DeleteMapping("/perfil/{id}")
-	public String deletarFreelancer(Long id){
-	 	freelancerService.deletaFreelancer(id);
-	 	return "redirect:/create-count";
-	}
-	
-	
-	
 	@PostMapping("/{servicoId}/enviar-curriculo")
 	public String enviarCurriculo(
 	        @PathVariable Long servicoId,
@@ -220,7 +208,6 @@ public class FreelancerController {
 	    return "freelancer-Visu-Proposta";
 	}
 
-	
 	
 }
 
